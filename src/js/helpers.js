8d3682e173e9d55970d6ac6c8403102084e8bf2f
Vladislav Kucherenko <vladislav.a.kucherenko@gmail.com>
2024-02-22 22:08:56 +0300

    feat: fn for distance added

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -12,6 +12,10 @@ function distBetweenCoordinates(x1, x2){
   return x2 - x1;
 }
 
+function distBetweenPoints(x1, y1, x2, y2){
+  return Math.sqrt(Math.pow(distBetweenCoordinates(x1, x2), 2) + Math.pow(distBetweenCoordinates(y1, y2), 2) )
+}
+
 function addClickElem(x, y){
   var newDiv = document.createElement("div");
   newDiv.classList.add('clickRed');
